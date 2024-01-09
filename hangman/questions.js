29629a8e420cce8f0304b97e65fd11a50a2e2161
Dasha <girl.is.anime20@gmail.com>
2024-01-09 16:10:08 +0300

    feat: add module questions.js

---
+++ hangman/questions.js
@@ -0,0 +1,41 @@
+let questions = [{
+  "question": "What animal lives only in China?",
+  "answer": "Panda"
+},
+{
+  "question": "What are the largest fish on the planet?",
+  "answer": "Shark"
+},
+{
+  "question": "What is the name of a bird with a bag under its beak?",
+  "answer": "Pelican"
+},
+{
+  "question": "What does the Earth's atmosphere mainly consist of?",
+  "answer": "Nitrogen"
+},
+{
+  "question": "What flower is the symbol of England?",
+  "answer": "Rose"
+},
+{
+  "question": "What flower can be called the national symbol of Russia??",
+  "answer": "Chamomile"
+},
+{
+  "question": "What is the central part of planet Earth?",
+  "answer": "Core"
+},
+{
+  "question": "Which ocean is the deepest?",
+  "answer": "Pacific"
+},
+{
+  "question": "What is the thickest tree on Earth?",
+  "answer": "Baobab"
+},
+{
+  "question": "Which country is the smallest on Earth?",
+  "answer": "Vatican"
+}
+]
