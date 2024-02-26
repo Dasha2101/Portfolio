ac0f58cb15175479e633d69f3a1e6cbe2e517a47
Dasha <girl.is.anime20@gmail.com>
2024-02-26 22:54:16 +0300

    feat: add enum

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -1,12 +1,17 @@
 import Loader from './loader';
 
+enum AppData {
+    url = 'https://newsapi.org/v2/',
+    keyAPI = 'b211ceefd3ce40429e1d1939eec094c9',
+}
+
 class AppLoader extends Loader {
     constructor() {
         if (process.env.API_URL === undefined) {
             throw new Error('API_URL is not defined');
         }
-        super(process.env.API_URL, {
-            apiKey: 'b211ceefd3ce40429e1d1939eec094c9',
+        super(AppData.url, {
+            apiKey: AppData.keyAPI,
         });
     }
 }
