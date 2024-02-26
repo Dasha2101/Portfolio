e77061fc560b0400affbe61ab4e2307858a13a7a
Dasha <girl.is.anime20@gmail.com>
2024-02-26 20:46:30 +0300

    feat: modicate appLoader

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -2,6 +2,9 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
+        if (process.env.API_URL === undefined) {
+            throw new Error('API_URL is not defined');
+        }
         super(process.env.API_URL, {
             apiKey: 'b211ceefd3ce40429e1d1939eec094c9',
         });
