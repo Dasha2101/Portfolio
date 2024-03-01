e4448c59826539303b8e2693ff1bf313ac9d2610
Dasha <girl.is.anime20@gmail.com>
2024-03-01 22:42:44 +0300

    fix: fixed the bug

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -1,15 +1,12 @@
 import Loader from './loader';
 
-enum AppData {
+const enum AppData {
     url = 'https://newsapi.org/v2/',
     keyAPI = 'b211ceefd3ce40429e1d1939eec094c9',
 }
 
 class AppLoader extends Loader {
     constructor() {
-        if (process.env.API_URL === undefined) {
-            throw new Error('API_URL is not defined');
-        }
         super(AppData.url, {
             apiKey: AppData.keyAPI,
         });
