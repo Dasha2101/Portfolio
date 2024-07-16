4270bccf25bbe0f1133a1b59b83c575ca827114a
Dasha <girl.is.anime20@gmail.com>
2024-07-16 10:41:13 +0300

    feat: add fisrt react project

---
+++ hooks-and-routing/src/App.tsx
@@ -0,0 +1,13 @@
+import RouterComponent from './router/Router';
+
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="app-container">
+      <RouterComponent />
+    </div>
+  );
+};
+
+export default App;
